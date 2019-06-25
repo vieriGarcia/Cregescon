c7f2ea138a8a36c18f4976a40decd7e7882fedf2
CristhianAlvarez1 <47466640+CristhianAlvarez1@users.noreply.github.com>
2019-06-25 18:40:37 -0500

    feat: manejando sidebar

---
+++ cregescon/src/java/pe/edu/unmsm/sistemas/servlets/sidebar.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.unmsm.sistemas.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Darkness
+ */
+public class sidebar extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession s = request.getSession(false);
+        String option = (String) s.getAttribute("sidebarOption");
+        String selected = request.getParameter("string");
+        s.setAttribute("sidebarOption", selected);
+        //response.sendRedirect(request.getContextPath() + "/index.jsp");
+    }
+}
